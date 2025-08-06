fa2358bacf4f0488bfacc8049e84d4e321af4949
Dusan <dusan.daniska@wai.sk>
2025-08-06 08:21:54 +0200

    added Invoices app

---
+++ src/Invoices/Loader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HubletoApp\Community\Invoices;
+
+class Loader extends \Hubleto\Framework\App
+{
+
+  public function init(): void
+  {
+    parent::init();
+
+    $this->main->router->httpGet([
+      '/^invoices\/?$/' => Controllers\Invoices::class,
+    ]);
+
+  }
+
+  public function installTables(int $round): void
+  {
+    if ($round == 1) {
+      (new Models\Invoice($this->main))->dropTableIfExists()->install();
+      (new Models\InvoiceItem($this->main))->dropTableIfExists()->install();
+    }
+  }
+
+}
---
+++ src/Invoices/Loader.tsx
@@ -0,0 +1,5 @@
+import InvoicesTableInvoices from "./Components/TableInvoices"
+import InvoicesTableInvoiceItems from "./Components/TableInvoiceItems"
+
+globalThis.main.registerReactComponent('InvoicesTableInvoices', InvoicesTableInvoices);
+globalThis.main.registerReactComponent('InvoicesTableInvoiceItems', InvoicesTableInvoiceItems);
